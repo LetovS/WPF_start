a8ec1cc80d45eeca7f11ce2ca421f0cd75655b41
Sergey Letov <letovs87@gmail.com>
2023-04-07 19:58:46 +0300

    Добавлена реализация команды

---
+++ HR/HR_desktop_app/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,26 @@
+﻿using HR_desktop_app.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_desktop_app.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecuted;
+
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecuted = null!)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecuted = CanExecuted;
+        }
+
+        public override bool CanExecute(object? parameter) => _CanExecuted?.Invoke(parameter!) ?? true;
+
+        public override void Execute(object? parameter) => _Execute(parameter!);
+    }
+}
