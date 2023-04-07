009ec5888fcdf792e00762c729d6862e88cf059b
Sergey Letov <letovs87@gmail.com>
2023-04-07 16:33:26 +0300

    Проверка работоспособности представления модели и представления

---
+++ HR/HR_desktop_app/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using HR_desktop_app.ViewModels.Base;
+using System;
+
+namespace HR_desktop_app.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        /// <summary> Заголовок окна </summary>
+        private string _Title = "Поле для проверки работы привязки";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+    }
+}
