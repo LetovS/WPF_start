9db4df1015a58a75d430af4196365b94f23a9683
Sergey Letov <letovs87@gmail.com>
2023-04-10 21:08:42 +0300

    Классы моделей представлений разнесены по разным файлам.

--- HR/HR_desktop_app/ViewModels/TreeDerictoriesViewModels/DirectoryViewModel.cs
+++ HR/HR_desktop_app/ViewModels/TreeDerictoriesViewModels/DirectoryViewModel.cs
@@ -29,13 +29,4 @@ namespace HR_desktop_app.ViewModels.TreeDerictoriesViewModels
         public DirectoryViewModel(string path) =>_DirectorInfo = new DirectoryInfo(path);
         
     }
-
-    internal class FileViewModel : ViewModel
-    {
-        private readonly FileInfo _FileInfo;
-        public FileViewModel(string path) => _FileInfo = new FileInfo(path);
-        public string Name => _FileInfo.Name;
-        public string Path => _FileInfo.FullName;
-        public DateTime CreationTime => _FileInfo.CreationTime;
-    }
 }
